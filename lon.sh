d761445e4ee634b7f5d21cefba22378cc2e6b3bd
Bela Ban <belaban@yahoo.com>
2012-12-13 13:12:06 +0100

    ns

--- lon.sh
+++ lon.sh
@@ -1,11 +1,12 @@
 
- #!/bin/bash
+#!/bin/bash
 
- ## Starts a node in the LON site
+## Starts a node in the LON site
 
- site=LON
- backupSites=SFO,NYC
+site=LON
+backupSites=SFO,NYC
 
- flags="-Djava.net.preferIPv4Stack=true -Dlog4j.configuration=file:/home/bela/log4j.properties"
- flags="$flags -Dsite=$site -DbackupSites=$backupSites"
- mvn $flags  exec:java -Dexec.mainClass=org.perf.Test
+flag="-Xms600m -Xmx600m"
+flags="$flags -Djava.net.preferIPv4Stack=true -Dlog4j.configuration=file:/home/bela/log4j.properties"
+flags="$flags -Dsite=$site -DbackupSites=$backupSites"
+mvn $flags  exec:java -Dexec.mainClass=org.perf.Test
