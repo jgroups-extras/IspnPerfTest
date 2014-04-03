fd027a083e7b55cbc12fff614d3ee296a0fd61d6
Bela Ban <belaban@yahoo.com>
2014-04-03 11:19:47 +0200

    Converted project from maven to ant&ivy

--- sfo.sh
+++ sfo.sh
@@ -12,4 +12,4 @@ flag="-Xms600m -Xmx600m"
 flags="$flags -Djava.net.preferIPv4Stack=true -Dlog4j.configuration=file:/home/bela/log4j.properties"
 flags="$flags -Dsite=$site -DbackupSites=$backupSites"
 flags="$flags -Dmcast_addr=$mcast_addr -Dmcast_port=$mcast_port"
-mvn $flags  exec:java -Dexec.mainClass=org.perf.Test
+java $flags -cp ./classes:./lib/*:./src/main/resources org.perf.Test
