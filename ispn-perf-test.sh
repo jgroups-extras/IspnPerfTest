86d0a0ebb7a7686dc8526b44131df8180fb020f1
Bela Ban <belaban@yahoo.com>
2014-06-26 13:34:50 +0200

    ns

---
+++ ispn-perf-test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+## Starts a local node using infinispan.xml as Infinispan config and local.xml as JGroups config
+
+PT=$HOME/IspnPerfTest
+
+CP=$PT/classes:$PT/lib/*:$PT/conf
+flag="-Xms1G -Xmx1G"
+flags="$flags -Djava.net.preferIPv4Stack=true -Dlog4j.configuration=file:/home/bela/log4j.properties"
+
+java $flags -cp $CP org.perf.Test $*
