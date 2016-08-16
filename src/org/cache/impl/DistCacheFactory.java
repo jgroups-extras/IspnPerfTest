0cf2d56e483f03cbcf6d972073668941aeb406a0
Bela Ban <belaban@yahoo.com>
2016-08-16 14:31:45 +0200

    Added DistCache which mimicks the way Infinispan DIST works (numOwners=2)

---
+++ src/org/cache/impl/DistCacheFactory.java
@@ -0,0 +1,33 @@
+package org.cache.impl;
+
+import org.cache.Cache;
+import org.cache.CacheFactory;
+import org.jgroups.util.Util;
+
+/**
+ * @author Bela Ban
+ * @since x.y
+ */
+public class DistCacheFactory<K,V> implements CacheFactory<K,V> {
+    protected String    config;
+    protected DistCache cache;
+
+    public void init(String config) throws Exception {
+        this.config=config;
+    }
+
+    public void destroy() {
+        Util.close(cache);
+    }
+
+    public Cache<K,V> create(String cache_name) {
+        try {
+            cache=new DistCache(config);
+            cache.setMcastPuts(false);
+            return cache;
+        }
+        catch(Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
