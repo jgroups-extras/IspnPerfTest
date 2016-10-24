386c3a2b8db0720f17c7d1eda0d49ba521854be8
Bela Ban <belaban@yahoo.com>
2016-10-24 12:59:08 +0200

    Changed num_keys to 100'000

--- src/org/perf/Test.java
+++ src/org/perf/Test.java
@@ -47,7 +47,7 @@ public class Test extends ReceiverAdapter {
 
     // ============ configurable properties ==================
     @Property protected int     num_threads=25;
-    @Property protected int     num_keys=50000, time_secs=30, msg_size=1000;
+    @Property protected int     num_keys=100000, time_secs=30, msg_size=1000;
     @Property protected double  read_percentage=0.8; // 80% reads, 20% writes
     @Property protected boolean print_details=true;
     @Property protected boolean print_invokers;
