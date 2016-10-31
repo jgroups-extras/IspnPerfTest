d10fa49337f121f83d7b170b7b9304e8272ed69d
Bela Ban <belaban@yahoo.com>
2016-10-31 12:44:24 +0100

    ns

--- bin/kill-all.sh
+++ bin/kill-all.sh
@@ -3,8 +3,7 @@
 ## Kills all Java processes for a given user on the hosts listed below
 
 CLUSTER=edg-perf
-COMMAND=perf-test.sh
 
-for i in 01 02 03 04 05 06 07 08;
+for i in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16;
   do ssh -f ${CLUSTER}${i} "killall java";
 done
