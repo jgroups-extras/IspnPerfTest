d15e1c0d020fed28bcee8bb4548f031d5b35cdd2
Bela Ban <belaban@yahoo.com>
2016-11-01 10:20:01 +0100

    ns

--- bin/run-on.sh
+++ bin/run-on.sh
@@ -3,7 +3,7 @@
 ## Runs a command on multiple hosts
 
 CLUSTER=edg-perf
-COMMAND=perf-test.sh
+COMMAND=${1:-perf-test.sh}
 
 for i in 02 03 04 05 06 07 08;
   do ssh -f ${CLUSTER}${i} "nohup IspnPerfTest/bin/${COMMAND} -nohup 2> /tmp/log < /dev/null &";
