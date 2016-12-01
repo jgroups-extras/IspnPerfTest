1d1ded298612d79ade5eb69ba371a91e90a76ab0
Bela Ban <belaban@yahoo.com>
2016-12-01 18:20:27 +0100

    ns

--- src/org/cache/impl/tri/TriCache.java
+++ src/org/cache/impl/tri/TriCache.java
@@ -46,7 +46,7 @@ public class TriCache<K,V> extends ReceiverAdapter implements Cache<K,V>, Closea
     // If true, a GET for key K is handled by the primary owner of K only, otherwise any owner for K can handle a GET(K)
     protected boolean                                  only_primary_handles_gets;
 
-    protected boolean                                  stats;
+    protected boolean                                  stats=true;
 
 
     // Maps req-ids to futures on which callers block (e.g. put() or get()) until an ACK has been received
