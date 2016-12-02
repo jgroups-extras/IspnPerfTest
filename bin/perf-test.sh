a8c7a98e48986d87c9829dc86baf721a32c68705
Bela Ban <belaban@yahoo.com>
2016-12-02 08:03:28 +0100

    ns

--- bin/perf-test.sh
+++ bin/perf-test.sh
@@ -21,6 +21,8 @@ if [ -f $HOME/logging.properties ]; then
 fi;
 
 
+### Note: change max heap to 2G on cluster01-08 (physical mem: 4G) !
+### On edg-perf, this is OK (physical mem: 32G)
 FLAGS="$FLAGS -server -Xms8G -Xmx8G"
 FLAGS="$FLAGS -Djava.net.preferIPv4Stack=true"
 JMC="-XX:+UnlockCommercialFeatures -XX:+FlightRecorder"
