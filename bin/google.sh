c7bd131406f09b17b1f0b59bfc213830069f53db
Bela Ban <belaban@yahoo.com>
2017-05-15 09:23:22 +0200

    Added scripts and configs to run on Google Compute Platform

---
+++ bin/google.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+# Author: Bela Ban
+
+DIR=`dirname $0`
+PT="$DIR/../"
+
+CP=$PT/classes:$PT/lib/*:$PT/conf
+
+if [ -f $HOME/log4j.properties ]; then
+    LOG="-Dlog4j.configuration=file:$HOME/log4j.properties"
+fi;
+
+if [ -f $HOME/log4j2.xml ]; then
+    LOG="$LOG -Dlog4j.configurationFile=$HOME/log4j2.xml"
+fi;
+
+if [ -f $HOME/logging.properties ]; then
+    LOG="$LOG -Djava.util.logging.config.file=$HOME/logging.properties"
+fi;
+
+
+### Note: change max heap to 2G on cluster01-08 (physical mem: 4G) !
+### On edg-perf, this is OK (physical mem: 32G)
+FLAGS="$FLAGS -server -Xms2G -Xmx2G"
+FLAGS="$FLAGS -Djava.net.preferIPv4Stack=true"
+
+#DEBUG="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8787"
+
+conf_dir=`dirname $0`/../conf
+exec java -classpath $CP $LOG $FLAGS $DEBUG org.perf.Test -cfg ${conf_dir}/dist-sync-google.xml -jgroups-cfg control-google.xml $*
