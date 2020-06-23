f52a62659c52aa2ceb75dcdb53fe71e70ced83ae
Bela Ban <belaban@mailbox.org>
2020-06-23 12:10:53 +0200

    Added dockerfile

--- bin/uperf.sh
+++ bin/uperf.sh
@@ -4,9 +4,18 @@
 # Author: Bela Ban
 
 DIR=`dirname $0`
-POM=$DIR/../pom.xml
+TARGET_DIR=$DIR/../target
+DEP=$TARGET_DIR/dependency
 
-CP=$PT/classes:$PT/lib/*:$PT/conf
+if [ ! -d $TARGET_DIR ]; then
+   echo "$TARGET_DIR not found; run build.sh first!"
+   exit 1
+fi
+
+if [ ! -d $DEP ]; then
+  echo "$DEP not found; run build.sh first!"
+  exit 1
+fi
 
 if [ -f $HOME/log4j.properties ]; then
     LOG="-Dlog4j.configuration=file:$HOME/log4j.properties"
@@ -20,14 +29,23 @@ if [ -f $HOME/logging.properties ]; then
     LOG="$LOG -Djava.util.logging.config.file=$HOME/logging.properties"
 fi;
 
+CP="$TARGET_DIR/classes:$DEP/*"
+
 
 ### Note: change max heap to 2G on cluster01-08 (physical mem: 4G) !
 ### On edg-perf, this is OK (physical mem: 32G)
 #FLAGS="$FLAGS -server -Xms2G -Xmx2G"
 FLAGS="$FLAGS -Djava.net.preferIPv4Stack=true"
 
-## If uncommented and used in prod, license fees may incur
-## JMC="-XX:+UnlockCommercialFeatures -XX:+FlightRecorder"
+## Delay asking backup for GET in Infinispan:
+FLAGS="$FLAGS -Dinfinispan.stagger.delay=5000"
+
+
+## Radargun JVM options (comment the above -Xms/-Xmx)
+#FLAGS="$FLAGS -Xms16g -Xmx16g -XX:+UseLargePages -XX:+UseG1GC -XX:MaxGCPauseMillis=300 -XX:InitiatingHeapOccupancyPercent=70"
+
+#FLAGS="$FLAGS -verbose:gc -XX:+PrintGCDateStamps -XX:+PrintGCDetails -Xloggc:gc-perf09.log -XX:+PrintTenuringDistribution -XX:+PrintGCApplicationStoppedTime -XX:+PrintGCCause"
+
 
 ## good flags: 112'000 reads/node ispn on edg-perf01-08
 # FLAGS="$FLAGS -XX:TLABSize=300k -XX:-ResizeTLAB"
@@ -48,17 +66,22 @@ JMX="-Dcom.sun.management.jmxremote"
 
 #java -Xrunhprof:cpu=samples,monitor=y,interval=5,lineno=y,thread=y -classpath $CP $LOG $FLAGS $JMX  $*
 
-#DEBUG="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5000"
+#DEBUG="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8787"
 #GC_LOG="-XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:$HOME/gclog.log"
 
 
-## BYTEMAN: uncomment the line below if you want to get delivery times (from reception of a message to delivery to the
-## application. Run probe.sh delivery / delivery-reset to get data
-#BM="-javaagent:$PT/lib/byteman.jar=script:$PT/conf/delivery.btm"
+## BYTEMAN: uncomment the line below if you want to get timings (avg-send-time, avg-receive-time, avg-delivery-time)
+## Run probe timings / timings-reset
+#BM="-javaagent:$PT/lib/byteman.jar=script:$CONF/delivery.btm,script:$CONF/send.btm,script:$CONF/requests.btm"
 
 # Uncomment to enable dtrace tracing on the hotspot provider (e.g. lock and method invocation tracing)
 #TRACE=-XX:+ExtendedDTraceProbes
 
-conf_dir=`dirname $0`/../conf
+export proc_id=$$
 
-exec mvn -o -f $POM exec:java $TRACE $DEBUG $LOG $FLAGS $JMX $JMC -Dexec.mainClass=org.jgroups.tests.perf.UPerf -Dexec.args="-props jgroups-udp.xml $*"
+#exec java $TRACE $CONFIG -classpath $CP -Dproc_id=${proc_id} $DEBUG $LOG $FLAGS $JMX $JMC $BM org.perf.Test $*
+
+# exec mvn -o -f $POM exec:java $FLAGS $JMX $LOG -Dexec.mainClass=org.perf.Test -Dexec.args="$*"
+
+java -cp $CP $FLAGS $DEBUG org.jgroups.tests.perf.UPerf $*
+
