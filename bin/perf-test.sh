df597c68ab02dff956256af755b90c0d1178972c
Bela Ban <belaban@mailbox.org>
2022-11-15 15:33:26 +0100

    Set Xmx Xms to 1GB

--- bin/perf-test.sh
+++ bin/perf-test.sh
@@ -34,7 +34,7 @@ FLAGS="$JAVA_OPTIONS"
 ### Note: change max heap to 2G on cluster01-08 (physical mem: 4G) !
 ### On edg-perf, this is OK (physical mem: 32G)
 ### May need to comment on small boxes!!
-FLAGS="$FLAGS -Xms8G -Xmx8G"
+FLAGS="$FLAGS -Xms1G -Xmx1G"
 FLAGS="$FLAGS -Djava.net.preferIPv4Stack=true"
 
 ## Delay asking backup for GET in Infinispan:
