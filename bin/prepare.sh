0e0eb3d4650723f944b5932088c06ba45f61991a
Bela Ban <belaban@mailbox.org>
2022-11-23 13:46:37 +0100

    ns

--- bin/prepare.sh
+++ bin/prepare.sh
@@ -20,11 +20,14 @@ if [ -f $ZIP ]; then
 else
    echo "$ZIP does not exist; creating it:"
    rm -f $DIR/hazel* $DIR/coher* $DIR/rocks*
-   zip -r9 $ZIP $ISPN_PERF -x $ISPN_PERF/.git/* $ISPN_PERF/.idea/*
+   zip -r9 $ZIP $ISPN_PERF -x \*.git\* \*.idea\*
 fi
 
 echo "-- Uploading artifacts to $HOST"
 
+rm -f $HOME/$ISPN_PERF/target/libs/hazel*
+rm -f $HOME/$ISPN_PERF/target/libs/coher*
+rm -f $HOME/$ISPN_PERF/target/libs/rocks*
 scp $ZIP $PUB_KEY $HOME/infinispan-core*.jar $HOME/$ISPN_PERF/bin/install.sh $HOME/$ISPN_PERF/bin/install2.sh root@$HOST:~/
 
 ssh root@${HOST} chmod +x install.sh
