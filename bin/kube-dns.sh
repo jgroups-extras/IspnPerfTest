11b288680b6b17f46aa07cfecf412ae2bc37b14e
Bela Ban <belaban@mailbox.org>
2023-01-12 10:05:24 +0100

    ns

--- bin/kube-dns.sh
+++ bin/kube-dns.sh
@@ -1,31 +1,5 @@
 #!/bin/bash
 
-
 # Author: Bela Ban
 
-DIR=`dirname $0`
-PT="$DIR/../"
-
-CP=$PT/classes:$PT/lib/*:$PT/conf
-
-if [ -f $HOME/log4j.properties ]; then
-    LOG="-Dlog4j.configuration=file:$HOME/log4j.properties"
-fi;
-
-if [ -f $HOME/log4j2.xml ]; then
-    LOG="$LOG -Dlog4j.configurationFile=$HOME/log4j2.xml"
-fi;
-
-if [ -f $HOME/logging.properties ]; then
-    LOG="$LOG -Djava.util.logging.config.file=$HOME/logging.properties"
-fi;
-
-
-### Note: change max heap to 2G on cluster01-08 (physical mem: 4G) !
-### On edg-perf, this is OK (physical mem: 32G)
-FLAGS="$FLAGS -server -Xms2G -Xmx2G"
-FLAGS="$FLAGS -Djava.net.preferIPv4Stack=true"
-
-#DEBUG="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=*:8787"
-
-exec `dirname $0`/perf-test.sh -cfg dist-sync-kube-dns.xml -jgroups-cfg control-kube-dns.xml $*
+`dirname $0`/perf-test.sh -cfg dist-sync-kube-dns.xml -control-cfg control-kube-dns.xml $*
