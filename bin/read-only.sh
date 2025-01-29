ac1492d8477a4acb8232f92a6910be6ad9ba27f2
Bela Ban <belaban@mailbox.org>
2025-01-29 09:09:51 +0100

    ns

---
+++ bin/read-only.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Illegal number of parameters: read-only.sh true|false"
+    exit 0
+fi
+
+DIR=`dirname $0`
+threads="$1"
+shift
+
+echo "enabling virtual threads: $threads"
+
+$DIR/run.sh -Dorg.infinispan.threads.virtual=$threads -Dvthreads=$threads \
+     org.perf.Test -cfg dist-sync-gcp.xml -control-cfg control-gcp.xml $*
