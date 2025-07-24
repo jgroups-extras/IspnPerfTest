f09e2ab7b6b3b344a819432fd66e2990fbe357bc
Bela Ban <belaban@mailbox.org>
2025-07-24 12:55:56 +0200

    ns

---
+++ bin/pp-results.sh
@@ -0,0 +1,22 @@
+
+## Pretty-print results.csv
+
+#!/bin/bash
+
+if [ $# -lt 1 ];
+    then echo "pp-results.sh <results.csv>";
+         exit 1
+fi
+
+echo $1
+
+RESULTS_FILE=$1
+
+cat $RESULTS_FILE | sed "s/-Dorg.infinispan.threads.virtual=True -Dvthreads=True //g" |
+    sed "s/jg-vthreads=true//g" | sed "s/ispn-vthreads=true//g" |
+    sed "s/.Final//g" | sed "s/-D//g" | sed "s/env-props=//g" |
+    sed "s/jgroups.bundler.//g" | sed "s/bundler.//g" | sed "s/use_//g" |
+    sed "s/single_sender_thread/sst/g" | sed "s/ringbuffer/rb/g" |
+    sed "s/rb=true/+rb/g" | sed "s/rb=false/-rb/g" |
+    sed "s/sst=true/+sst/g" | sed "s/sst=false/-sst/g" | sed "s/type=//g" |
+     tr -s ' '
