ed4c5149ec1c108456defa74b84700bdc121619f
github-action-benchmark <github@users.noreply.github.com>
2026-03-24 06:34:16 +0000

    add BelasTest (customBiggerIsBetter) benchmark result for 1d3d8ffac14d23981b1c1dc9b193400b4c0946a8

---
+++ dev/benchmark/data.js
@@ -0,0 +1,36 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1774334056122,
+  "repoUrl": "https://github.com/jgroups-extras/IspnPerfTest",
+  "entries": {
+    "BelasTest": [
+      {
+        "commit": {
+          "author": {
+            "name": "José Bolina",
+            "username": "jabolina",
+            "email": "jose@infinispan.org"
+          },
+          "committer": {
+            "name": "José Bolina",
+            "username": "jabolina",
+            "email": "jose@infinispan.org"
+          },
+          "id": "1d3d8ffac14d23981b1c1dc9b193400b4c0946a8",
+          "message": "Allow concurrent benchmark actions",
+          "timestamp": "2026-03-23T15:16:16Z",
+          "url": "https://github.com/jgroups-extras/IspnPerfTest/commit/1d3d8ffac14d23981b1c1dc9b193400b4c0946a8"
+        },
+        "date": 1774334055720,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Cluster Throughput",
+            "value": 193032.88,
+            "unit": "reqs/sec",
+            "extra": " \n4 nodes, dist-sync, virtual threads (JDK: 25, ISPN: 16.0.5, JGroups: 5.5.4)"
+          }
+        ]
+      }
+    ]
+  }
+}
